be3013d5aab2287966d9baf5785679e0ff2149bc
Shaurya1409 <shauryasrivastava14@gmail.com>
2020-08-18 00:22:34 +0530

    Update Year 4 - All New Projects Added

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -21,6 +21,49 @@ function toPage3(){
     $("#Page3").show(600)
 }
 
+
+function toAcadBack(){
+    event.preventDefault();
+    $("#Page2").hide(600);
+    $("#Page3").hide(600);
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    $("#Page1").show(600);
+    var element = document.getElementById("Academics");
+    element.scrollIntoView();
+}
+
+function toAchievements(){
+    event.preventDefault();
+    var element = document.getElementById("Achievements");
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function toSkills(){
+    event.preventDefault();
+    var element = document.getElementById("Skills");
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function toProjects(){
+    event.preventDefault();
+    var element = document.getElementById("Projects");
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function toResume(){
+    event.preventDefault();
+    var element = document.getElementById("Resume");
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+function toContact(){
+    event.preventDefault();
+    var element = document.getElementById("footer");
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+
+
 function loadPage(link){
     event.preventDefault();
     linkLocation = link
